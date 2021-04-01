01767a8145a9d03d10657f3d162e135fe9519381
Надежда Шемер <shemernm.20@edu.ystu.ru>
2021-04-01 11:08:26 +0400

    '

---
+++ Калькулятор/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Калькулятор
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           while (true)
+           {
+            double a, b;
+            string c;
+           
+             try
+             {
+                Console.WriteLine("Введите первую переменную: ");
+                a = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите вторую переменную: ");
+                b = double.Parse(Console.ReadLine());
+             }
+             catch(Exception)
+             {
+                Console.WriteLine("Нет такого числа. Выберите другое ");
+                Console.ReadLine();
+                continue;
+             }
+
+             Console.WriteLine("Выбирите действие: 1) сложение 2)выситание 3) умножение 4) деление ");
+             c = Console.ReadLine();
+
+                switch (c)
+                {
+                    case "1":
+                        Console.WriteLine( a + b);
+                        break;
+
+                    case "2":
+                        Console.WriteLine(a - b);
+                        break;
+
+                    case "3":
+                        Console.WriteLine(a * b);
+                        break;
+
+                    case "4":
+                        if (b == 0)
+                            Console.WriteLine("На 0 делить нельзя");
+                        else
+                            Console.WriteLine(a / b);
+                        break;
+
+                    default:
+                        Console.WriteLine("Попробуйте еще раз.");
+                        break;
+
+                }
+
+                Console.ReadLine();
+            }
+
+            
+        }    
+    }
+}
