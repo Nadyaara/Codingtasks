a39f60f2ae31a3adfdfd1894e931ff675c84f411
Надежда Шемер <shemernm.20@edu.ystu.ru>
2021-04-01 09:05:09 +0400

    Добавьте файлы проекта.

---
+++ NOD NOK/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace NOD_NOK
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                int a, b, c;
+                try
+                {
+                    Console.Write("Введите первое число: ");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Введите второе число: ");
+                    b = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Введите третье число: ");
+                    c = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Нет такого числа. Выберите другое ");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                Console.WriteLine("НОД={0}", nod(a, b, c));
+                Console.WriteLine("НОК={0}", nok(a, b, c));
+                Console.ReadKey();
+            }
+        }
+
+        static double nod(double a, double b, double c)
+        {
+            if (b < 0)
+                b = -b;
+            if (a < 0)
+                a = -a;
+            if (c < 0)
+                c = -c;
+            while (b > 0)
+            {
+                double temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+         }
+         static double nok(double a, double b, double c)
+         {
+                return Math.Abs(a*b*c)/nod(a, b, c);
+         }         
+     
+    }
+    
+}
+ 
+ 
---
+++ Блокнот/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Блокнот
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Color(object sender, RoutedEventArgs e)
+        {
+            TextBox.Foreground = Foreground.;
+        }
+
+        private void Shrift(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Text(DependencyProperty property, object value)
+            {
+                if (value!= null)
+                {
+                    TextBox.SelectionBrush.ApplyAnimationClock (property, null);
+                }
+            }
+
+        private void Clean(object sender, RoutedEventArgs e) => TextBox.Clear();
+
+        private void Exit(object sender, RoutedEventArgs e) => Close();
+
+        private void Color1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Color2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Shrift1(object sender, RoutedEventArgs e) => TextBox.FontStyle = FontStyles.Italic;
+
+        private void Shrift2(object sender, RoutedEventArgs e)
+        {
+            TextBox.FontStyle = FontStyles.
+        }
+
+        private void Text1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Text2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
