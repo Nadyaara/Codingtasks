648df0f3afc770af829e73e2778fd40a818e8e1f
Надежда Шемер <shemernm.20@edu.ystu.ru>
2021-06-10 15:16:15 +0400

    '

---
+++ Геометрические фигуры/Геометрия/Cylinder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_6_circles
+{
+    class Cylinder:ICylinder
+    {
+
+        private double _radius;
+        private double _height;
+
+        public void SetHeight(double height)
+        {
+            _height = height;
+        }
+
+        public void SetRadius(double radius)
+        {
+            _radius = radius;
+        }
+
+        public double Volume()
+        {
+            return _radius * _radius * _height * Math.PI;
+        }
+    }   
+}
---
+++ Геометрические фигуры/Геометрия/Sfera.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_6_circles
+{
+     class Sfera:Isfera
+    {
+        private double _radius;
+
+        public void SetRadius(double radius)
+        {
+            _radius = radius;
+        }
+
+        public double Volume()
+        {
+            return 4 / 3 * Math.PI * _radius * _radius * _radius;
+        }
+
+    }
+}
